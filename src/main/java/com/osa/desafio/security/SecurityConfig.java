dac9c26ca90d03dc0c1f27bce75dc7f4d99653e7
renanp96 <renanpa1996@gmail.com>
2025-09-08 23:46:17 -0300

    atualizadas as permissoes de acesso as APIs de documentacao

--- src/main/java/com/osa/desafio/security/SecurityConfig.java
+++ src/main/java/com/osa/desafio/security/SecurityConfig.java
@@ -12,7 +12,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/desafio/**").permitAll()
+                        .requestMatchers("/swagger-ui/**", "/v3/api-docs/**", "/desafio/**").permitAll()
                         .anyRequest().authenticated()
                 );
         return http.build();
